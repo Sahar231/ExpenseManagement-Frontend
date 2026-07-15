08577c85492f1c77883cd5593b89c5d1689f0f01
sahar231 <Riabi.Sahar@esprit.tn>
2026-07-15 11:24:28 +0100

    Fix sigup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login';
+import { SignupComponent } from './pages/auth/signup/signup'; // 1. Importez le composant Signup
 import { DashboardComponent } from './pages/dashbord/dashbord';
 import { FraisCreateComponent } from './pages/frais/create/create';
 import { ListeComponent } from './pages/frais/liste/liste';
-import { ValidationComponent } from './pages/frais/validation/validation'; // 1. Importez le composant
+import { ValidationComponent } from './pages/frais/validation/validation';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }, // 2. Ajoutez la route ici
   { 
     path: 'dashboard', 
     component: DashboardComponent,
@@ -16,8 +18,7 @@ export const routes: Routes = [
       { path: 'frais/liste', component: ListeComponent },
       { 
         path: 'frais/validation', 
-        component: ValidationComponent,
-        // canActivate: [authGuard] // Ajoutez votre guard ici pour la sécurité
+        component: ValidationComponent
       }
     ]
   }
